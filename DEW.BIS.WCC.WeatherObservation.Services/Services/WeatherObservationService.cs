1cfe8e47c9a3612f3426efab080e7b7362959076
pep30cula <amirbijou@gmail.com>
2024-06-24 20:45:35 +0930

    Add project files.

---
+++ DEW.BIS.WCC.WeatherObservation.Services/Services/WeatherObservationService.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DEW.BIS.WCC.WeatherObservation.Services.Services
+{
+    public class WeatherObservationService
+    {
+
+    }
+}
